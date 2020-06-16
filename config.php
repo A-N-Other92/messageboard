300ea7500e201b99c6654ae852b7f59f17bc38e4
A-N-Other92 <A-N-Other92@users.noreply.github.com>
2020-06-16 21:48:25 +0100

    Tidied up files

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params - Put above the htdocs folder for extra security
+  define("DB_HOST", "Your host url");
+  define("DB_USER", "Your user name");
+  define("DB_PASS", "Your password");
+  define("DB_NAME", "Your database name");
+
+  // URL Root
+  define('URLROOT', 'Your website URL');
+
+  
+
+?>  
